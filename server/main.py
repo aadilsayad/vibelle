7ffb45e60112d8a0c765bcc2f4eb5f93dc4c7062
Aadil Sayad <aadilsayad@gmail.com>
2024-12-05 23:45:38 +0530

    feat: add PostgreSQL and SQLAlchemy configurations

--- server/main.py
+++ server/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
-@app.get('/')
-def test():
-    return 'Hello World'
+DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/vibelle'
+
+engine = create_engine(DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db = sessionLocal()
+
+
+class UserSignup(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+@app.post('/signup')
+def signup_user(signup_request: UserSignup):
+    # extract the data from request body
+    print(signup_request.name)
+    print(signup_request.email)
+    print(signup_request.password)
+    # check if useer already exists in the db
+    # add user to the db
+    pass
